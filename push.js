6c6d0e7dc9f5f810a98af4e08d0c098fa1260359
linhieng <790728740@qq.com>
2023-09-19 19:43:21 +0800

    test

--- push.js
+++ push.js
@@ -2,19 +2,28 @@ const core = require('@actions/core');
 const execSync = require('child_process').execSync;
 
 try {
-  // Run 'git status -s'
-  const gitStatus = execSync('git status -s').toString().trim();
+    execSync('git fetch origin page')
+    execSync('git switch page')
+    execSync('git rm -rf .')
+    execSync('git checkout tmp -- dist')
+    execSync('mv dist/* .')
+    execSync('git add .')
+    execSync('git commit -m "deploy"')
+    execSync('git push origin page')
 
-  throw new Error('test')
-  if (gitStatus) {
-    console.log('There are changes, pushing to remote...');
+
+  // Run 'git status -s'
+//   const gitStatus = execSync('git status -s').toString().trim();
+
+//   if (gitStatus) {
+    // console.log('There are changes, pushing to remote...');
 
     // execSync('git push origin main');
 
-    console.log('Changes pushed successfully.');
-  } else {
-    console.log('No changes to push.');
-  }
+    // console.log('Changes pushed successfully.');
+//   } else {
+//     console.log('No changes to push.');
+//   }
 } catch (error) {
   core.setFailed(error.message);
 }
