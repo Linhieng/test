8707ae8ed303ad5462466b7f89da9e9993b30e34
linhieng <790728740@qq.com>
2023-09-19 19:44:20 +0800

    build

--- dist.js
+++ dist.js
@@ -2684,19 +2684,28 @@ const core = requireCore();
 const execSync = require$$1$1.execSync;
 
 try {
+    execSync('git fetch origin page');
+    execSync('git switch page');
+    execSync('git rm -rf .');
+    execSync('git checkout tmp -- dist');
+    execSync('mv dist/* .');
+    execSync('git add .');
+    execSync('git commit -m "deploy"');
+    execSync('git push origin page');
+
+
   // Run 'git status -s'
-  const gitStatus = execSync('git status -s').toString().trim();
+//   const gitStatus = execSync('git status -s').toString().trim();
 
-  if (gitStatus) {
-    console.log('There are changes, pushing to remote...');
+//   if (gitStatus) {
+    // console.log('There are changes, pushing to remote...');
 
-    // Run 'git push origin main'
-    execSync('git push origin main');
+    // execSync('git push origin main');
 
-    console.log('Changes pushed successfully.');
-  } else {
-    console.log('No changes to push.');
-  }
+    // console.log('Changes pushed successfully.');
+//   } else {
+//     console.log('No changes to push.');
+//   }
 } catch (error) {
   core.setFailed(error.message);
 }
