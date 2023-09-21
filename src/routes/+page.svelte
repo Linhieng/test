0a52ca460fbabd5f8f8b148f40e1ef249433be40
linhieng <790728740@qq.com>
2023-09-21 21:42:31 +0800

    over test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,5 @@
-<script>
-    /**
-     * @type {string}
-     */
-    let value
-</script>
-
 <svelte:head>
-    <title>随心敲</title>
+    <title>TEST - 随心敲</title>
 </svelte:head>
 
-<h1>Welcome to My Blog</h1>
-<div>
-    <input type="text" bind:value />
-    <p>{value}</p>
-</div>
+<h1>This is a test page</h1>
