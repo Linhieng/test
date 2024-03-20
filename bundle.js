5f10775ad08197bf14d63a7a5f621ffcaca847bf
linhieng <790728740@qq.com>
2024-03-20 17:17:34 +0800

    test

--- bundle.js
+++ bundle.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 
 if (fs.existsSync('dist')) {
     fs.rmSync('dist', {recursive: true})
-    fs.mkdirSync('dist')
 }
+fs.mkdirSync('dist')
 
 // 模拟打包行为
 fs.writeFileSync('dist/hello.txt', 'hello=world', 'utf8')
